73fd89c6bab70153819b3e0174bcc0fcfbfe808c
Pankajakshan Vineeth <yamunapankaj99@gmail.com>
2025-11-26 16:26:24 +0530

    Navbar Completed

--- src/compnents/Navbar/Navbar.jsx
+++ src/compnents/Navbar/Navbar.jsx
@@ -1,15 +1,38 @@
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [selectedMenu, setSelectedMenu] = useState("menu");
+
   return (
     <div className="navbar">
       <img src={assets.logo} className="logo" />
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile App</li>
-        <li>Contact Us</li>
+        <li
+          className={selectedMenu === "home" ? "active" : ""}
+          onClick={() => setSelectedMenu("home")}
+        >
+          Home
+        </li>
+        <li
+          className={selectedMenu === "menu" ? "active" : ""}
+          onClick={() => setSelectedMenu("menu")}
+        >
+          Menu
+        </li>
+        <li
+          className={selectedMenu === "mobile-app" ? "active" : ""}
+          onClick={() => setSelectedMenu("mobile-app")}
+        >
+          Mobile App
+        </li>
+        <li
+          className={selectedMenu === "contact-us" ? "active" : ""}
+          onClick={() => setSelectedMenu("contact-us")}
+        >
+          Contact Us
+        </li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} />
